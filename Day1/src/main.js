dc722a8e75a6e0dcf423923d062f5eedf602fb45
Bleon5--Github--Username <bleonarllati2@gmail.com>
2025-01-29 13:14:57 +0100

    Day 1 material

---
+++ Day1/src/main.js
@@ -0,0 +1,25 @@
+console.log("Hello, world!");
+
+// Data types
+// Primitive data types
+
+// String
+("I'm Bleon");
+// Numbers
+10;
+// Boolean
+false;
+true;
+// Bigint
+909205203523n;
+// Undefined
+// Null
+// Symbol
+
+// Declaring a variable
+const firstName = "Bleon";
+console.log(firstName);
+let age = "23";
+console.log(age);
+var profession = "Programmer";
+console.log(profession);
